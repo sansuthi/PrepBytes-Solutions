cfa4493568522ab90d36139c48bfa10c167a7add
SANDHYA S <84490912+sansuthi@users.noreply.github.com>
2021-08-02 18:39:23 +0530

    30/7/2021 11:53PM

---
+++ Fundamentals/Reverse_the_Number.py
@@ -0,0 +1,23 @@
+# Reverse the number
+'''
+PrepBuddy gave you a number X and asks you to reverse that number and print it.
+
+Input format
+One integer is provided denoting the value of X.
+Note - The given number doesn't have leading or ending zero's.
+
+Output format
+Print the reverse of X.
+
+Constraints
+1 <= X <= 10^6
+
+Example
+Input
+102345
+
+Output
+543201
+'''
+# SOLUTION - 30/7/2021 11:53PM
+print(input()[::-1])
