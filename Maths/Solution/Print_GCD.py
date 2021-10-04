3340a3167100708679110415d4c776fbcad06d84
SANDHYA S <84490912+sansuthi@users.noreply.github.com>
2021-10-04 10:14:17 +0530

    4/10/2021 10:09AM

---
+++ Maths/Solution/Print_GCD.py
@@ -0,0 +1,34 @@
+# Print GCD
+'''
+Let's begin with a very simple question. 
+You are given two integers represented by N and M, your task is to print GCD(Greatest Common Divisor) of N and M.
+
+Input format
+The first line contains an integer T denoting the number of test cases.
+For the next T lines, each line contains two integers represented by N and M.
+
+Output format
+For each test case on a new line, print GCD of N and M.
+
+Constaints
+. 1 <= T <= 10
+. 1 <= N <= 10^5
+. 1 <= M <= 10^5
+
+Example
+Input
+2
+4 10
+6 9
+
+Output
+2
+3
+'''
+# SOLUTION - 4/10/2021 10:09AM	
+for _ in range(int(input())):
+  n, m = map(int, input().split())
+  for i in range(1, min(n, m)+1):
+    if n % i == 0 and m % i == 0:
+      gcd = i
+  print(gcd)
