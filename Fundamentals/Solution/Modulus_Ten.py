4ee7bdd3c98049071d4138fed6fbe80d629b0a5c
SANDHYA S <84490912+sansuthi@users.noreply.github.com>
2022-03-20 22:25:34 +0530

    20/3/2022 10:05PM

---
+++ Fundamentals/Solution/Modulus_Ten.py
@@ -0,0 +1,28 @@
+# Modulus Ten
+'''
+PrepBuddy gave you a number x, and asked you to find the remainder of x divided by 10.
+
+Input format
+A single integer x is provided.
+
+Output format
+Print a single integer representing the required output.
+
+Constraints
+. 1 <= x <= 2000
+
+Time Limit
+1 second
+
+Example
+Input
+1345
+
+Output
+5
+
+Sample Test Case Explanation
+1345/10 = 134 with remainder as 5.
+'''
+# SOLUTION - 20/3/2022 10:05PM
+print(int(input())%10)
