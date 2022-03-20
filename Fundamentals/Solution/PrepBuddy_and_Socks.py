d2621c844eaeeeb3a3d6bc741254952949097a2e
SANDHYA S <84490912+sansuthi@users.noreply.github.com>
2022-03-20 19:28:58 +0530

    20/3/2022 7:25PM

---
+++ Fundamentals/Solution/PrepBuddy_and_Socks.py
@@ -0,0 +1,36 @@
+# PrepBuddy and Socks
+'''
+PrepBuddy is getting late for college. She is looking for a matching pair of socks from a box full of socks. 
+In the worst-case scenario, how many socks should PrepBuddy remove from her drawer until she finds a matching pair.
+
+Input format
+The first line contains the number of test cases T. 
+Next T lines contain an integer N which indicates the total pairs of socks present in the drawer.
+
+Output format
+Print the number of draws PrepBuddy will have to make in the worst case.
+
+Constraints
+. 1 <= T <= 1000
+. 1 <= N < 10^6
+
+Time Limit
+1 second
+
+Example
+Input
+2
+1
+2
+
+Output
+2
+3
+
+Sample Test case Explanation
+In the first test case, there is just 1 pair of socks i.e 2 socks in her drawer, 
+so she has to remove both to get a pair, therefore the output is 2.
+'''
+# SOLUTION - 20/3/2022 7:25PM
+for _ in range(int(input())):
+  print(int(input())+1)
