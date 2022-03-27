6e5ede32b220a0766708be9233ad37c915f6ce8e
SANDHYA S <84490912+sansuthi@users.noreply.github.com>
2022-03-27 10:44:16 +0530

    27/3/2022 10:40AM

--- Fundamentals/Solution/Sorcerer_Sequence.py
+++ Fundamentals/Solution/Sorcerer_Sequence.py
@@ -19,6 +19,9 @@ Constraints
 . 1 <= T <= 100
 . 1 <= N <= 100
 
+Time Limit
+1 second
+
 Example
 Input1
 2
@@ -33,10 +36,14 @@ Explanation
 In the first test case, we start with 8 and use the above formula to get the next terms, until we reach 1.
 In the second test case, we start with 5, next digit is (sqrt(5)*sqrt(5)*sqrt(5)) = 11, and so on.
 '''
-# SOLUTION - 19/8/2021 6:00PM
+# SOLUTION - 27/3/2022 10:40AM
 for _ in range(int(input())):
-  array = [int(input())]
-  while array[-1] != 1:
-    if array[-1] % 2 == 0: array.append(int(array[-1]**0.5))
-    else: array.append(int(array[-1]**1.5))
-  print(*array)
+  num = int(input());
+  print(num, end=' ');
+  while num != 1:
+    if num % 2 == 0: 
+      num = int(num**0.5);
+    else: 
+      num = int(num**1.5);
+    print(num, end=' ');
+  print();
