045c3d464e3830e1816106407e5df8e551168872
SANDHYA S <84490912+sansuthi@users.noreply.github.com>
2022-04-09 12:16:21 +0530

    9/4/2022 12:15PM

--- Arrays/Solution/Min_and_Max.py
+++ Arrays/Solution/Min_and_Max.py
@@ -12,10 +12,13 @@ Output format
 For each test case on a new line, print minimum and maximum elements separated by space.
 
 Constraints
-1 <= T <= 5
-1 <= N < 10^7
-0 <= A[i] <= 10^7
+. 1 <= T <= 5
+. 1 <= N < 10^7
+. 0 <= A[i] <= 10^7
 
+Time limit
+1 second
+ 
 Example
 Input
 2
@@ -32,8 +35,14 @@ Sample Test Case Explanation
 In the first test case, minimum element = 1 and maximum element = 7
 In the second test case, minimum element = 0 and maximum element = 0
 '''
-# SOLUTION - 31/7/2021 2:10PM
+# SOLUTION - 9/4/2022 12:15PM
 for _ in range(int(input())):
-  n = input()
-  array = list(map(int, input().split()))
-  print(min(array), max(array))
+  N = int(input());
+  A = list(map(int, input().split()));
+  minimum = maximum = A[0];
+  for i in range(1, N):
+    if A[i] < minimum:
+      minimum = A[i];
+    if A[i] > maximum:
+      maximum = A[i];
+  print(minimum, maximum);
